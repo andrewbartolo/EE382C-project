a91cd7e5ba32946000d1f20bdb43405f392f65d7
Andrew Bartolo <bartolo@stanford.edu>
2024-03-10 17:09:02 -0700

    Enable parallel simulation with N_WORKER_THREADS in simulate.py

--- simulate.py
+++ simulate.py
@@ -2,14 +2,16 @@
 import os
 import subprocess
 from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
 
 BOOKSIM_EXE = './booksim2/src/booksim'
 BASE_CFG = './base.cfg'
 OUT_DIR = './sim-out'
+N_WORKER_THREADS = 16
 
 def run_cmd(booksim_exe, base_cfg, out_dir, k, packet_size, traffic, rate):
     prefix = Path(out_dir, f'K{k}-S{packet_size}-T{traffic}-R{int(rate*100):03d}')
-    print(f"simulating {prefix}...")
+    print(f"launching {prefix}...")
 
     m_path = prefix.with_suffix('.m')
     stdout_path = prefix.with_suffix('.out')
@@ -26,8 +28,12 @@ def run_cmd(booksim_exe, base_cfg, out_dir, k, packet_size, traffic, rate):
     with open(stdout_path, 'w') as f:
         f.write(res.stdout)
 
+    print(f"    completed {prefix}.")
+
 def main():
     os.makedirs(OUT_DIR, exist_ok=True)
+
+    tp = ThreadPool(max_workers=N_WORKER_THREADS)
 
     # sweep over the following parameters
     for k in [4, 8]:
@@ -35,7 +41,10 @@ def main():
             # tornado exhibited some weird results; stick with 'uniform' and 'neighbor' for now
             for traffic in ['uniform', 'neighbor']:
                 for rate in [x/100 for x in range(5, 100, 5)]:
-                    run_cmd(BOOKSIM_EXE, BASE_CFG, OUT_DIR, k, packet_size, traffic, rate)
+                    # NOTE: all submitted threads will implicitly be joined before the program exits
+                    # (https://docs.python.org/3/library/concurrent.futures.html#threadpoolexecutor)
+                    tp.submit(run_cmd, BOOKSIM_EXE, BASE_CFG, OUT_DIR, k, packet_size, traffic,
+                              rate)
 
 
 if __name__ == '__main__':
